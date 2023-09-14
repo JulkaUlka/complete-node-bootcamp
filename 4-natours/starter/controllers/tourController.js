f527729c7920719ec1cce870b95d0c8785d366d4
JulkaUlka <y.glushko@outlook.com>
2023-09-14 22:54:54 +0300

    add field limiting

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -20,6 +20,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //4)Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     const tours = await query;
     // const tours = await Tour.find();
     res.status(200).json({
